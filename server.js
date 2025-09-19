49d3eafc440e6026020e64fd9ec4a718331f7022
Mina <minashahzoda@gmail.com>
2025-09-19 19:49:01 +0900

    feat: develop author routor page

--- server.js
+++ server.js
@@ -2,13 +2,23 @@ console.log(("Web Serverni boshlash"));
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if (err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+})
 
 // 1 Kirish kodlari
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// 2: Session code
+// 2: Session code-
 
 // 3 Views code
 app.set("views", "views");
@@ -16,17 +26,21 @@ app.set("view engine", "ejs");
 
 // 4 Routing code
 app.post("/create-item", (req, res) => {
-    console.log(req.body);
     res.json({ test: "success" })
+});
+
+app.get('/author', (req, res) => {
+    res.render("author", { user: user })
 })
-app.get('/', function(req, res) {
+
+app.get('/', function (req, res) {
     res.render('xarid');
-})
+});
 
 
 const server = http.createServer(app);
 let PORT = 3000;
-server.listen(PORT, function() {
+server.listen(PORT, function () {
     console.log(`The server is running successfully on port: ${PORT}`);
-    
+
 })

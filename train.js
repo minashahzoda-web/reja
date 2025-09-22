84f2fc27e541115dd720ab4783e66792e1f9ca6e
Mina <minashahzoda@gmail.com>
2025-09-22 14:42:02 +0900

    feat: learn callback, async functions and create countLetter function

--- train.js
+++ train.js
@@ -31,32 +31,32 @@
 // });
 
 
-console.log("Jack Ma maslahatlari");
-const list = [
-    "yaxshi talaba boling", // 0-20
-    "togri boshliq tanlang va koproq xato qiling", // 20-30
-    "ozingizga ishlashni boshlang", // 30-40
-    "siz kuchli bolgan narsalarni qiling", // 40-50
-    "yoshlarga investitsiya qiling", // 50-60
-    "endi dam oling foydasi yoq endi", // 60
-];
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//     "yaxshi talaba boling", // 0-20
+//     "togri boshliq tanlang va koproq xato qiling", // 20-30
+//     "ozingizga ishlashni boshlang", // 30-40
+//     "siz kuchli bolgan narsalarni qiling", // 40-50
+//     "yoshlarga investitsiya qiling", // 50-60
+//     "endi dam oling foydasi yoq endi", // 60
+// ];
 
-async function maslahatBering(a) {
-    if(typeof a !== 'number') throw new Error("insert a number");
-    else if(a <= 20) return list[0];
-    else if(a > 20 && a <= 30) return list[1];
-    else if(a > 30 && a <= 40) return list[2];
-    else if(a > 40 && a <= 50) return list[3];
-    else if(a > 50 && a <= 60) return list[4];
-    else {
-    //    return list[5];
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(list[5]);
-        }, 5000);
-    });
-    }
-}
+// async function maslahatBering(a) {
+//     if(typeof a !== 'number') throw new Error("insert a number");
+//     else if(a <= 20) return list[0];
+//     else if(a > 20 && a <= 30) return list[1];
+//     else if(a > 30 && a <= 40) return list[2];
+//     else if(a > 40 && a <= 50) return list[3];
+//     else if(a > 50 && a <= 60) return list[4];
+//     else {
+//     //    return list[5];
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(list[5]);
+//         }, 5000);
+//     });
+//     }
+// }
 
 // //then/catch
 // console.log("passed here 0");
@@ -69,13 +69,29 @@ async function maslahatBering(a) {
 // console.log('passed here 1');
 
 
-//async/await
-async function run() {
-    let javob = await maslahatBering(25);
-    console.log(javob);
-    javob = await maslahatBering(75);
-    console.log(javob);
-    javob = await maslahatBering(41);
-    console.log(javob);
+// //async/await
+// async function run() {
+//     let javob = await maslahatBering(25);
+//     console.log(javob);
+//     javob = await maslahatBering(75);
+//     console.log(javob);
+//     javob = await maslahatBering(41);
+//     console.log(javob);
+// }
+// run();
+
+
+let newArray = 0;
+function countLetter(letter, word) {
+    newArray = word.split("").filter(element => element.toLowerCase() === letter.toLowerCase());
+
+    if(newArray.length == 0) {
+        console.log("Topilmadi");
+    }
+         
+        return {
+            newArray
+        };
 }
-run();
+countLetter("i", "MIT")
+console.log(`Harflar: ${newArray}, soni ${newArray.length}`);
